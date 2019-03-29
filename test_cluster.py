0aa1b5d8de9ec2500b91c66246507de2dd2f9131
Lamine <lemzoba@gmail.com>
2019-03-29 08:03:50 +0100

    refactor: add unit tests for the cluster

--- test_cluster.py
+++ test_cluster.py
@@ -1,15 +1,20 @@
-from cluster import create_cluster
+from unittest.mock import patch
+
+from cluster import create_cluster as create
 
 
 class TestCluster:
-    def test_should_return_cluster_with_details(self):
-        # Given
-        environment = 'DEV'
-        vm_profile = 'small.1vCPU.1GoRAM'
-        version = '10.0'
+    def setup(self):
+        # Given params
+        self.environment = 'DEV'
+        self.vm_profile = 'small.1vCPU.1GoRAM'
+        self.version = '10.0'
+        build_name = patch('cluster.build_name')
+        self.build_name = build_name.start()
 
+    def test_should_return_cluster_with_details(self):
         # When
-        cluster = create_cluster(environment, vm_profile, version)
+        cluster = create(self.environment, self.vm_profile, self.version)
 
         # Then
         assert cluster.uuid is not None
@@ -19,3 +24,12 @@ class TestCluster:
         assert cluster.server_name is not None
         expected_hostname = f'cluster-pg-10-dev-{cluster.id[:8]}.cloud.com'
         assert cluster.hostname == expected_hostname
+
+    def test_should_call_build_name_with_right_params(self):
+        # When
+        cluster = create(self.environment, self.vm_profile, self.version)
+
+        # Then
+        self.build_name.assert_called_once_with(environment=self.environment,
+                                                db_engine='POSTGRESQL',
+                                                hostname=cluster.hostname)
